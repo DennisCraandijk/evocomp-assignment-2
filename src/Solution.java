b66727a0ccdcefe4f57bf7806bd887949a57a1d7
Dennis Craandijk <dennis_craandijk@hotmail.com>
2017-03-23 13:05:35 +0100

    Improve Clone method

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
@@ -10,26 +12,78 @@ import java.util.concurrent.ThreadLocalRandom;
 public class Solution implements Cloneable {
 
 
-    public int fitnessValue = 0;
+    public int fitness;
 
 
     public List<Integer> bitArray;
 
+    public List<List<Integer>> colorIndex;
+
+
+    /**
+     * Construct with random solution
+ * @param bitArray
+     */
+    public Solution(List<Integer> bitArray) {
+        this(bitArray, 0);
+    }
+
     /**
      * Construct with random solution
      */
-    public Solution(List bitArray) {
+    public Solution(List<Integer> bitArray, int fitness) {
         this.bitArray = bitArray;
+        this.fitness = fitness;
+        this.colorIndex = constructColorIndex(bitArray);
+    }
+
+    private List<List<Integer>> constructColorIndex (List<Integer> bitArray){
+
+        List<List<Integer>> colorIndex = new ArrayList<>(2);
+
+        List<Integer> verticesColored0 = new ArrayList<>(bitArray.size() / 2);
+        List<Integer> verticesColored1 = new ArrayList<>(bitArray.size() / 2);
+
+        for(int i = 0; i < bitArray.size(); i++) {
+            if(bitArray.get(i).intValue() == 0) {
+                verticesColored0.add(i);
+            } else {
+                verticesColored1.add(i);
+            }
+        }
+
+        colorIndex.add(0, verticesColored0);
+        colorIndex.add(1, verticesColored1);
+
+        return colorIndex;
     }
 
     /**
      * Swap vertexes on the n'the vertex with a zero and the Nth vertex with a one
-     *
-     * @param nthZero
-     * @param nthOne
+     * @param iZero
+     * @param iOne
      */
-    public void vertexSwap(int nthZero, int nthOne) {
+    public void vertexSwap(int iZero, int iOne) {
 
+        // swap vertices in the colorIndex
+        int vertexId0 = colorIndex.get(0).get(iZero);
+        int vertexId1 = colorIndex.get(1).get(iOne);
+
+        colorIndex.get(0).set(iZero, vertexId1);
+        colorIndex.get(1).set(iOne, vertexId0);
+
+        // swap vertices in the bitArray
+        bitArray.set(vertexId0, 1);
+        bitArray.set(vertexId1, 0);
+
+        return;
+
+    }
+
+
+    /*public void vertexSwap(int nthZero, int nthOne) {
+
+        //TODO change to using 2 dimensional color array
         int zeroCount = 0;
         int iZero = 0;
 
@@ -58,13 +112,18 @@ public class Solution implements Cloneable {
             }
         }
 
-    }
+    }*/
 
     @Override
     public Solution clone() {
         try {
             final Solution result = (Solution) super.clone();
             // copy fields that need to be copied here!
+            result.bitArray = new ArrayList<>(bitArray);
+            result.fitness = fitness;
+            result.colorIndex = new ArrayList<>(2);
+            result.colorIndex.add(0, new ArrayList<>(colorIndex.get(0)));
+            result.colorIndex.add(1, new ArrayList<>(colorIndex.get(1)));
             return result;
         } catch (final CloneNotSupportedException ex) {
             throw new AssertionError();
